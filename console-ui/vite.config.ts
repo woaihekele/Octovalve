ccba79c892387845577cc0e2b9e8b7879803b093
jie.yao <21751058@zju.edu.cn>
2025-12-27 00:34:50 +0800

    fix: rewrite console ui api proxy

--- console-ui/vite.config.ts
+++ console-ui/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:19309',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/ws': {
         target: 'ws://127.0.0.1:19309',
